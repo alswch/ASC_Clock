1f8209bbf9aec2dadf3020b60c24686fc15cf3e8
alswch <alswchung@gmail.com>
2017-06-05 15:55:12 -0400

    converted numbers to hexdecimal

--- clock_script.js
+++ clock_script.js
@@ -1,5 +1,6 @@
 window.setInterval(digitalclock,1000)
 
+
 function digitalclock () {
   var today = new Date();
   // console.log(today);
@@ -7,44 +8,44 @@ function digitalclock () {
   var m = today.getMinutes();
   var s = today.getSeconds();
 
+    h = checkTime(h);
     m = checkTime(m);
     s = checkTime(s);
-      function checkTime(i) {
-        // console.log("checkTime");
-        if (i < 10) {
-          i = "0" + i;
-        }
-        return i;
-    }
+
   document.getElementById('real-hour').innerHTML = h;
   document.getElementById('real-minute').innerHTML = m;
   document.getElementById('real-second').innerHTML = s;
 
 
 // ===== hex clock ======
-  var hex_h = parseInt(h);
-  var hex_m = parseInt(m);
-  var hex_s = parseInt(s);
-  // console.log(hex_h);
-  // console.log(hex_m);
-  // console.log(hex_s);
+  var hH = hexTime(h);
+  var mH = hexTime(m);
+  var sH = hexTime(s);
 
-    hex_m = checkTime(hex_m);
-    hex_s = checkTime(hex_s);
-      function checkTime(e) {
-        // console.log("checkTime");
-        if (e < 10) {
-          e = "0" + e;
-        }
-        return e;
+  hH = checkTime(hH);
+  mH = checkTime(mH);
+  sH = checkTime(sH);
+
+  // var color = "#" + hex_h + hex_m + hex_s;
+  // console.log(color);
+  // document.body.style.background = color;
+
+  document.getElementById('hex-hour').innerHTML = hH;
+  document.getElementById('hex-minute').innerHTML = mH;
+  document.getElementById('hex-second').innerHTML = sH;
+
+    function hexTime (num) {
+      hexString = num.toString(16);
+      console.log(hexString);
+      return hexString;
     }
 
-  var color = "#" + hex_h +hex_m + hex_s;
-  console.log(color);
-  document.body.style.background = color;
-
-  document.getElementById('hex-hour').innerHTML = hex_h;
-  document.getElementById('hex-minute').innerHTML = hex_m;
-  document.getElementById('hex-second').innerHTML = hex_s;
+  function checkTime(i) {
+    // console.log("checkTime");
+    if (i.length < "2") {
+      i = "0" + i;
+    }
+    return i;
+  }
 
 }
